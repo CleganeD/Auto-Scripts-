c2f9d2c78faccd2d11f97f21cbc56e59fc8e2d91
CleganeD <33360726+CleganeD@users.noreply.github.com>
2019-04-08 14:50:16 +0200

    Auto Delete Files In Folder Script Added
    Added Script that deletes content of a within code specified location. 
    Deletes files: .txt, .bmp, .doc.   More can be added.

---
+++ Auto Delete.bat
@@ -0,0 +1,21 @@
+REM Change to folder part of code:
+
+c:
+
+cd users
+
+cd jpsk
+
+cd desktop
+
+cd Temp
+
+
+
+REM Delete Part Of Code:
+
+DEL *.txt REM Delete .txt files, Text files.
+DEL *.bmp REM Delete .bmp files, Image files.
+DEL *.doc REM Delete .doc files, Word files.
+
+
